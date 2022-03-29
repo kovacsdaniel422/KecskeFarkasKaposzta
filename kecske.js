363e321efab9a5587f4715dc7e08bb835bb2c8c7
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-29 10:12:50 +0200

    something

--- kecske.js
+++ kecske.js
@@ -7,23 +7,11 @@ function CLASS(elem) {
   return document.getElementsByClassName(elem);
 }
 
-var kepeim = [
-  {
-    eleres: "kepek/csonak.png",
-    cim: "csonak",
-  },
-  {
-    eleres: "kepek/farkas.png",
-    cim: "farkas",
-  },
-  {
-    eleres: "kepek/kaposzta.png",
-    cim: "kaposzta",
-  },
-  {
-    eleres: "kepek/kecske.png",
-    cim: "kecske",
-  },
-];
+function init(){
+}
+
+function kattintKozep(){
+    ID('').id="jobbkecske"
 
 
+}
