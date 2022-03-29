14a39a5b3706c4a741c508cd99e665d8e10eecda
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-29 09:02:33 +0200

    js és egyéb fileok létrehozva

---
+++ kecske.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", init);
+
+function ID(elem) {
+  return document.getElementById(elem);
+}
+function CLASS(elem) {
+  return document.getElementsByClassName(elem);
+}
+
+var kepeim = [
+  {
+    eleres: "kepek/csonak.png",
+    cim: "csonak",
+  },
+  {
+    eleres: "kepek/farkas.png",
+    cim: "farkas",
+  },
+  {
+    eleres: "kepek/kaposzta.png",
+    cim: "kaposzta",
+  },
+  {
+    eleres: "kepek/kecske.png",
+    cim: "kecske",
+  },
+];
